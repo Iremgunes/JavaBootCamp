0cbd4b122051d334917ac9ae859723cd2cc21313
Iremgunes <aviremdanacioglu@gmail.com>
2022-08-08 21:44:10 +0300

    class and object

---
+++ src/day01/ClassAndObject.java
@@ -0,0 +1,33 @@
+package day01;
+
+import java.time.LocalDate;
+
+class Person{
+    public String name;
+    public char gender;
+    public LocalDate DOB;
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", DOB=" + DOB +
+                '}';
+    }
+
+    public Person() {
+    }
+
+
+}
+
+
+
+
+public class ClassAndObject {
+
+
+
+}
